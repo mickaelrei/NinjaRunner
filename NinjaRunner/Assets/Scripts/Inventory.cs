538ea7915a80e6407f07eaafb06d39c94b7db0b1
mickaelrei <mickaelreichert@gmail.com>
2022-08-31 20:26:01 -0300

    Adicionando shuriken

---
+++ NinjaRunner/Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Transform> weaponsList = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("")) {
+            
+        }
+    }
+}
